8b9e1d47d817082c30fe29a5100c2543cdfbb1f1
ecki <ecki@ecki-netz.de>
2024-10-06 23:26:18 +0200

    changed print statements to propper logging

--- vault_sqlite.py
+++ vault_sqlite.py
@@ -2,10 +2,11 @@ import copy
 import datetime
 import os
 import sqlite3 as sqlite
+import logging
 
 
 class VaultDBDict:
-    def __init__(self, filename, db_layout, password="none", debug=False):
+    def __init__(self, filename, db_layout, password="none"):
         """Try to smooth out handling sqlite as dict with get and set methods
             :param filename: name of the sql file
             :type filename: str
@@ -15,7 +16,8 @@ class VaultDBDict:
             :type password: str
 
         """
-        self.debug = debug
+        self.password = password
+        self.logger = logging.getLogger(__name__)
         # check if db file exist
         self.db_exists = os.path.exists(str(filename))
         self.db_layout = db_layout
@@ -25,17 +27,13 @@ class VaultDBDict:
         self.tables = []
         self.data = {}
         self.analyse_db_from_json()
-        self.__print("opening db {}".format(self.filename))
+        self.logger.info("try to open db file {}".format(self.filename))
         self.db_conn = sqlite.connect(self.filename, check_same_thread=False)
         self.curser = self.db_conn.cursor()
         if not self.db_exists:
-            self.__print("db {} does not exist - creating".format(filename))
+            self.logger.info("db file {} does not exist - creating".format(filename))
             self.create_tables()
         # self.get_data()
-
-    def __print(self, text):
-        if self.debug:
-            print(text)
 
     def create_db_string_from_json(self, table, json_str):
         """Create table using given parameter
@@ -46,11 +44,11 @@ class VaultDBDict:
             :return: SQL command
             :rtype: str
         """
-        self.__print("create table {}".format(table))
+        self.logger.info("create table {}".format(table))
         db_str = "CREATE TABLE {} (".format(table)
         for key in json_str:
             attribute = json_str.get(key)
-            self.__print("table: {}; key: {}; value: {}".format(table, key, attribute))
+            self.logger.debug("table: {}; key: {}; value: {}".format(table, key, attribute))
             if type(attribute) is dict:
                 for attribute in json_str.get(key, {}):
                     table_str = json_str.get(key, {})
@@ -61,6 +59,7 @@ class VaultDBDict:
         return db_str
 
     def create_table_primary(self, table):
+        self.logger.debug("generate primary entry for table {} creation".format(table))
         primary = self.primary.get(table, False)
         return_string = ""
         if type(primary) is list:
@@ -71,7 +70,6 @@ class VaultDBDict:
         return return_string
 
     def create_table_column(self, column, attribute, table):
-        # " {} {},".format(attribute, table_str.get(attribute))
         primary = self.primary.get(table, False)
         attribute = attribute.upper()
         if type(primary) is list:
@@ -84,13 +82,13 @@ class VaultDBDict:
 
     def analyse_db_from_json(self):
         """Read the given parameters, search for primary key"""
-        self.__print("analyse db layout from json")
+        self.logger.debug("analyse db layout from json")
         for table in self.db_layout:
             self.tables.append(table)
             keys = []
             for key, value in self.db_layout.get(table).items():
                 keys.append(key)
-                self.__print("table: {}; key: {}; value: {}".format(table, key, value))
+                self.logger.debug("table: {}; key: {}; value: {}".format(table, key, value))
                 if "PRIMARY KEY" in value:
                     primary = self.primary.get(table, False)
                     if not primary:
@@ -100,14 +98,14 @@ class VaultDBDict:
                             primary = [primary]
                         primary.append(key)
                     self.primary.update({table: primary})
-            self.__print("primary key(s) for table {} are {}".format(table, self.primary.get(table, False)))
+            self.logger.debug("primary key(s) for table {} are {}".format(table, self.primary.get(table, False)))
             self.keys.update({table: keys})
 
     def create_tables(self):
         """Create tables for all layouts"""
         for table in self.db_layout:
             create_str = self.create_db_string_from_json(table, self.db_layout.get(table, ""))
-            self.__print("create string for db: {}".format(create_str))
+            self.logger.debug("create string for db: {}".format(create_str))
             self.use_db(create_str)
 
     def disassemble_return_value(self, rdata, table):
@@ -148,7 +146,7 @@ class VaultDBDict:
             :return: got tables
             :rtype: dict
         """
-        self.__print("getdata")
+        self.logger.debug("getdata for table(s)")
         for table in self.db_layout:
             cmd = "SELECT "
             primary = self.primary.get(table, False)
@@ -163,7 +161,7 @@ class VaultDBDict:
             cmd = cmd[:-2]
             cmd += " FROM {}".format(table)
             rdata = self.disassemble_return_value(self.use_db(cmd).fetchall(), table)
-            self.__print("fetchall data: {}".format(rdata))
+            self.logger.debug("fetchall data: {}".format(rdata))
             self.data.update({table: rdata})
         if table_name:
             return self.data.get(table_name, {})
@@ -179,10 +177,10 @@ class VaultDBDict:
             :return: data from db
         """
         if not data:
-            self.__print("execute: {}".format(cmd))
+            self.logger.debug("execute: {}".format(cmd))
             return_value = self.curser.execute(cmd)
         else:
-            self.__print("execute: {}, {}".format(cmd, data))
+            self.logger.debug("execute: {}, {}".format(cmd, data))
             return_value = self.curser.execute(cmd, data)
         self.db_conn.commit()
         return return_value
@@ -193,7 +191,7 @@ class VaultDBDict:
         # {("primary_key1", "primary_key_2"): True}
         del_key = list(del_data_dict.keys())[0]
         if del_key in self.data.get(table, {}).keys():
-            self.__print("data exists - delete first")
+            self.logger.debug("key exists - delete")
             primary = self.primary.get(table, False)
             cmd_str = "DELETE FROM {} WHERE ".format(table)
             if type(primary) is not list:
@@ -218,17 +216,17 @@ class VaultDBDict:
             :rtype: int
         """
         if new_data == {}:
-            self.__print("Primary key {} for table {} not set in new data".format(self.primary.get(table, False),
-                                                                                  table))
+            self.logger.info("Primary key {} for table {} not set in new data".format(self.primary.get(table,
+                                                                                                       False), table))
         else:
             if self.data.get(table, {}) == {}:
                 self.get_data()
             new_key = list(new_data.keys())[0]
             new_value = list(new_data.values())[0]
-            self.__print("delete")
+            self.logger.debug("delete entry if entry with new key already exists")
             existing_data = copy.deepcopy(self.data.get(table, {}).get(new_key, {}))
             self.del_data(new_data, table)
-            self.__print("insert")
+            self.logger.debug("insert entry {}")
             insert_primary_str, insert_primary_value_str, insert_primary_value = self.insert_primary(table, new_key)
             insert_str = "INSERT INTO {} ({}".format(table, insert_primary_str)
             value_str = " VALUES ({}".format(insert_primary_value_str)
@@ -271,6 +269,8 @@ class VaultDBDict:
 
 if __name__ == "__main__":
     path_dir = "/tmp"
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
 
     access_db_layout = {"user": {"uid": "TEXT NOT NULL PRIMARY KEY", "name": "TEXT NOT NULL", "create_date": "TEXT"},
                         "lock": {"mac": "TEXT NOT NULL PRIMARY KEY", "name": "TEXT NOT NULL", "create_date": "TEXT",
@@ -280,34 +280,34 @@ if __name__ == "__main__":
                         "log": {"uid": "TEXT NOT NULL PRIMARY KEY", "date": "TEXT", "user": "TEXT NOT NULL",
                                 "lock": "TEXT NOT NULL", "accessed": "INTEGER"}}
     access_db_filename = os.path.join(path_dir, "example_access.db")
-    access_db = VaultDBDict(access_db_filename, access_db_layout, debug=False)
+    access_db = VaultDBDict(access_db_filename, access_db_layout)
 
-    print("Access DB")
+    logger.info("Access DB")
 
     # users
     new_user = {"id_1": {"test": "test stuff"}}
     access_db.set_data(new_user, table="user")
-    print("users: {}".format(access_db.get_data("user")))
+    logger.info("users: {}".format(access_db.get_data("user")))
     time_str = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
     new_user = {"id_1": {"create_date": time_str, "name": "Hans"}}
     access_db.set_data(new_user, table="user")
-    print("users: {}".format(access_db.get_data("user")))
+    logger.info("users: {}".format(access_db.get_data("user")))
 
     # locks
-    print("locks: {}".format(access_db.get_data("lock")))
+    logger.info("locks: {}".format(access_db.get_data("lock")))
     new_lock = {"mac_address_1": {"name": "front door", "create_date": "2024-09-24", "last_seen": "2024-10-01"}}
     access_db.set_data(new_lock, table="lock")
     new_lock = {"mac_address_2": {"name": "garage door", "create_date": "2024-09-24", "last_seen": "2024-10-01"}}
     access_db.set_data(new_lock, table="lock")
-    print("locks: {}".format(access_db.get_data("lock")))
+    logger.info("locks: {}".format(access_db.get_data("lock")))
 
     # access rights
-    print("access rights: {}".format(access_db.get_data("access")))
+    logger.info("access rights: {}".format(access_db.get_data("access")))
     new_access = {("id_1", "mac_address_1"): {"access": True}}
     access_db.set_data(new_access, table="access")
     new_access = {("id_1", "mac_address_2"): {"access": False}}
     access_db.set_data(new_access, table="access")
-    print("access rights: {}".format(access_db.get_data("access")))
+    logger.info("access rights: {}".format(access_db.get_data("access")))
 
     # logs
     import time
